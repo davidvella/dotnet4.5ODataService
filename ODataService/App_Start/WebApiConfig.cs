6c6d246703ebe0e0e38997f1e5d38b2d4b25294a
David Vella <david@vella.uk.com>
2020-02-05 15:35:13 +0000

    Initial Release

---
+++ ODataService/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.OData.Extensions;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using ODataService.Models;
+
+namespace ODataService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<HrEmployee>("HrEmployees");
+            config.MapODataServiceRoute(routeName: "ODataRoute", routePrefix: "api", model: builder.GetEdmModel());
+            // Enable odata values
+            config.Select().Expand().Filter().OrderBy().MaxTop(null).Count();
+        }
+    }
+}
---
+++ ODataService/Controllers/HrEmployeesController.cs
@@ -0,0 +1,144 @@
+﻿using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNet.OData;
+using ODataService.Models;
+
+
+namespace ODataService.Controllers
+{
+    public class HrEmployeesController : ODataController
+    {
+        private readonly DatabaseEntities _db = new DatabaseEntities();
+
+        // GET: odata/HrEmployees
+        [EnableQuery(PageSize = 30)]
+        public IQueryable<HrEmployee> GetHrEmployees()
+        {
+            return _db.HrEmployees;
+        }
+
+        // GET: odata/HrEmployees(5)
+        [EnableQuery]
+        public SingleResult<HrEmployee> GetHrEmployee([FromODataUri] int key)
+        {
+            return SingleResult.Create(_db.HrEmployees.Where(hrEmployee => hrEmployee.ID == key));
+        }
+
+        // PUT: odata/HrEmployees(5)
+        public async Task<IHttpActionResult> Put([FromODataUri] int key, Delta<HrEmployee> patch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            HrEmployee hrEmployee = await _db.HrEmployees.FindAsync(key);
+            if (hrEmployee == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(hrEmployee);
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HrEmployeeExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(hrEmployee);
+        }
+
+        // POST: odata/HrEmployees
+        public async Task<IHttpActionResult> Post(HrEmployee hrEmployee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _db.HrEmployees.Add(hrEmployee);
+            await _db.SaveChangesAsync();
+
+            return Created(hrEmployee);
+        }
+
+        // PATCH: odata/HrEmployees(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public async Task<IHttpActionResult> Patch([FromODataUri] int key, Delta<HrEmployee> patch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            HrEmployee hrEmployee = await _db.HrEmployees.FindAsync(key);
+            if (hrEmployee == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(hrEmployee);
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HrEmployeeExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(hrEmployee);
+        }
+
+        // DELETE: odata/HrEmployees(5)
+        public async Task<IHttpActionResult> Delete([FromODataUri] int key)
+        {
+            HrEmployee hrEmployee = await _db.HrEmployees.FindAsync(key);
+            if (hrEmployee == null)
+            {
+                return NotFound();
+            }
+
+            _db.HrEmployees.Remove(hrEmployee);
+            await _db.SaveChangesAsync();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool HrEmployeeExists(int key)
+        {
+            return _db.HrEmployees.Count(e => e.ID == key) > 0;
+        }
+    }
+}
