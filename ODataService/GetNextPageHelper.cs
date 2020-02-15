22b401744cc71b7fa88e148f11bb6faf4e0342ac
David Vella <david@vella.uk.com>
2020-02-15 16:35:49 +0000

    Add odata pagination helper

---
+++ ODataService/GetNextPageHelper.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace ODataService
+{
+    public class GetNextPageHelper
+    {
+        internal static Uri GetNextPageLink(Uri requestUri, IEnumerable<KeyValuePair<string, string>> queryParameters, int pageSize)
+        {
+            Contract.Assert(requestUri != null);
+            Contract.Assert(queryParameters != null);
+
+            StringBuilder queryBuilder = new StringBuilder();
+
+            int nextPageSkip = pageSize;
+
+            foreach (KeyValuePair<string, string> kvp in queryParameters)
+            {
+                string key = kvp.Key.ToLowerInvariant();
+                string value = kvp.Value;
+
+                switch (key)
+                {
+                    case "$top":
+                        if (int.TryParse(value, out var top))
+                        {
+                            // We decrease top by the pageSize because that's the number of results we're returning in the current page. If the $top query option's value is less than or equal to the page size, there is no next page.
+                            if (top > pageSize)
+                            {
+                                value = (top - pageSize).ToString(CultureInfo.InvariantCulture);
+                            }
+                            else
+                            {
+                                return null;
+                            }
+                        }
+                        break;
+                    case "$skip":
+
+                        //Need to increment skip only if we are not using skiptoken 
+                        if (int.TryParse(value, out var skip))
+                        {
+                            // We increase skip by the pageSize because that's the number of results we're returning in the current page
+                            nextPageSkip += skip;
+                        }
+
+                        continue;
+                }
+
+                if (key.Length > 0 && key[0] == '$')
+                {
+                    // $ is a legal first character in query keys
+                    key = '$' + Uri.EscapeDataString(key.Substring(1));
+                }
+                else
+                {
+                    key = Uri.EscapeDataString(key);
+                }
+
+                value = Uri.EscapeDataString(value);
+
+                queryBuilder.Append(key);
+                queryBuilder.Append('=');
+                queryBuilder.Append(value);
+                queryBuilder.Append('&');
+            }
+
+            queryBuilder.AppendFormat("$skip={0}", nextPageSkip);
+
+            UriBuilder uriBuilder = new UriBuilder(requestUri)
+            {
+                Query = queryBuilder.ToString()
+            };
+
+            return uriBuilder.Uri;
+        }
+
+        internal static Uri GetNextPageLink(HttpContext request, int pageSize)
+        {
+            return GetNextPageLink(request.Request.Url, ToPairs(request.Request.QueryString), pageSize);
+        }
+
+        public static IEnumerable<KeyValuePair<string, string>> ToPairs(NameValueCollection collection)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            return collection.Cast<string>().Select(key => new KeyValuePair<string, string>(key, collection[key]));
+        }
+    }
+}
